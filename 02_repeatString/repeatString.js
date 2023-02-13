8192dd0822977539d00c374a9312f59a5a136e97
Gradnos <sdolaqidze@gmail.com>
2023-02-13 20:08:19 +0400

    Solve 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(repString, times) {
+    if (times < 0) return "ERROR";
+    let finalString = '';
+    for(i=0; i<times; i++) finalString+=repString;
+    return finalString;
 };
 
 // Do not edit below this line
