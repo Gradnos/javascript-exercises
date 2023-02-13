94736c22f0d7af3bbfcb2cddb0b75b8d28d9c66c
Gradnos <sdolaqidze@gmail.com>
2023-02-13 20:44:41 +0400

    Solve 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,8 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+    if (typeof int1 !== "number" || typeof int2 !== "number" || int2 < 0 || int1 < 0) return "ERROR";
+    let avgValue = (int1+int2)/2;
+    let amount = Math.abs(int2 - int1) + 1;
+    return avgValue * amount;
 };
 
 // Do not edit below this line
