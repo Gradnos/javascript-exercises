22c23535ff97a3d2143e3743544aaab4c505a6be
Gradnos <sdolaqidze@gmail.com>
2023-02-13 20:33:15 +0400

    Solve 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(givenArray, ...unwanted) {
+    for(i = 0; i < givenArray.length; i++){
+        for(j = 0; j <unwanted.length; j++){
+            if (givenArray[i] === unwanted[j]) {
+                givenArray.splice(i, 1);
+                i--;
+                break;
+            }
+        }
+    }
+    return givenArray;
 };
 
 // Do not edit below this line
