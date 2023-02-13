3b06b8cdd4741da88600627cae3720c16422d310
Gradnos <sdolaqidze@gmail.com>
2023-02-13 20:14:04 +0400

    Solve 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(startString) {
+    let finishedString = '';
+    for (i = startString.length - 1; i>=0; i--) finishedString+=startString[i];
+    return finishedString;
 };
 
 // Do not edit below this line
