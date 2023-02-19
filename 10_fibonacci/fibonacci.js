eebb315d471547e8908888d417a60282a5e852dd
Gradnos <sdolaqidze@gmail.com>
2023-02-19 20:35:20 +0400

    Solve 10 fibbonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    if(a<0) return "OOPS";
+    fib = [1,1];
+    for (i = 2; i < a; i++){
+        fib[i] = fib[i-1]+fib[i-2];
+    }
+    return fib[fib.length-1];
 };
 
 // Do not edit below this line
