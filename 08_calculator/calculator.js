db14642f9938cfaee1f57c5c5f20469323153f2a
Gradnos <sdolaqidze@gmail.com>
2023-02-19 20:19:10 +0400

    Solve 08 calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  let num = a.reduce((plus, number) =>{
+    return plus +  (+number);
+  }, 0);
+
+
+
+  //return(a[0]);
+  return num;
 };
 
-const multiply = function() {
+const multiply = function(a) {
+  let num = a.reduce((plus, number) =>{
+    return plus *  (+number);
+  }, 1);
 
+  return num;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if(a===0) return 1;
+   let fact = 1;
+	for(i=1; i<=a; i++) fact *= i;
+  return fact;
 };
 
 // Do not edit below this line
