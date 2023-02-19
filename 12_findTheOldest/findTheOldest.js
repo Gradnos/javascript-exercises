34744f6cedf3cf21302057567a51311334e08501
Gradnos <sdolaqidze@gmail.com>
2023-02-19 21:55:17 +0400

    Solve 12 oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(a) {
+const date= new Date().getFullYear();
+let oldest = a.reduce((old, person) =>{
+    if(old === 0) old = person;
+    let time;
+    if (person.yearOfDeath === undefined) person.yearOfDeath = date;
+    if ((person.yearOfDeath-person.yearOfBirth) > (old.yearOfDeath-old.yearOfBirth)) return person;
+    return old;
+}, 0);
 
+return oldest;
 };
 
 // Do not edit below this line
