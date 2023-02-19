499804cd368417b72c520dbf72dac73bce4b10a9
Gradnos <sdolaqidze@gmail.com>
2023-02-19 20:30:26 +0400

    Solve 09 palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (string) {
+string = string.toLowerCase().replace(/[^\w\s\']|_/g, "").replace(/\s/g, "");
 
+let splitStr = string.split("");
+let reverseArray = splitStr.reverse();
+let srtingReverse = reverseArray.join("");
+return string === srtingReverse;
 };
 
 // Do not edit below this line
